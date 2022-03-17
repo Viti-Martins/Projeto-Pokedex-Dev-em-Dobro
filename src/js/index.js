842362fdcf49e0811f4eaeaecf7a3c743da47ee7
Vitor Martins <adv.vmartins96@gmail.com>
2022-03-17 12:19:03 -0400

    Add a pasta JS + Criei uma lista de passos a serem seguidos por comentários
    Foi criada a pasta js na pasta src, foi criado o arquivo index.js, e foi escrito uma série de passos a serem seguidos por meio de comentário no arquivo js

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+/*
+quando clicar no pokémon da listagem temos que esconder o cartão do pokémon aberto e mostrar o cartão corresponden ao que foi selecionado na listagem.
+
+para isso vamos precisar trabalhar com dois elementos: 
+1- lsitagem; 
+2- cartão do pokemon.
+
+precisamos criar duas variavéis no JS para trabalhar com os elementos da tela
+
+vamos precisar trabalhar om um evento de clique feito pelo usuário na listagem de pokémons
+
+- remover a classe aberto só do cartão que está aberto;
+- ao clicar em um pokémon da listagem pegamos o id desse pokémon para saber qual cartão mostrar;
+- remover a classe ativo que marca o pokémon selecionado;
+- adicionar a classe ativo no item da lista selecionado.
+
+*/
+
+// precisamos criar duas variáveis no js para trabalhar com os elementos da tela
+
