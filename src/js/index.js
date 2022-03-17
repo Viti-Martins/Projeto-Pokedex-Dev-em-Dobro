0451643d749c2e0258de5cfed0411d39d49d06f9
Vitor Martins <adv.vmartins96@gmail.com>
2022-03-17 15:10:57 -0400

    Adição de variáveis e eventos DOM durante a aula
    Adição de variáveis e eventos DOM durante a aula.

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,11 @@
 quando clicar no pokémon da listagem temos que esconder o cartão do pokémon aberto e mostrar o cartão corresponden ao que foi selecionado na listagem.
 
 para isso vamos precisar trabalhar com dois elementos: 
-1- lsitagem; 
+1- listagem; 
 2- cartão do pokemon.
 
 precisamos criar duas variavéis no JS para trabalhar com os elementos da tela
 
-vamos precisar trabalhar om um evento de clique feito pelo usuário na listagem de pokémons
-
-- remover a classe aberto só do cartão que está aberto;
-- ao clicar em um pokémon da listagem pegamos o id desse pokémon para saber qual cartão mostrar;
 - remover a classe ativo que marca o pokémon selecionado;
 - adicionar a classe ativo no item da lista selecionado.
 
@@ -18,3 +14,22 @@ vamos precisar trabalhar om um evento de clique feito pelo usuário na listagem 
 
 // precisamos criar duas variáveis no js para trabalhar com os elementos da tela
 
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach(pokemon => {
+
+    //vamos precisar trabalhar om um evento de clique feito pelo usuário na listagem de pokémons.
+
+    pokemon.addEventListener('click', () => {
+        // - remover a classe aberto só do cartão que está aberto;
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+
+        //- ao clicar em um pokémon da listagem pegamos o id desse pokémon para saber qual cartão mostrar;
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const cartaoPokemonParaAbrir = document.getElementById('cartao-' + idPokemonSelecionado)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+    })
+})
